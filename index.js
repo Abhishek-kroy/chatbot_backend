b57f746bd7b168c00b1aaf84b32aee27e1f1049b
Abhishek <abhishekroy2342005@gmail.com>
2025-06-13 20:35:01 +0530

    Voice Response completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://rnchatbot.netlify.app/'],
+  origin: ['http://localhost:5173', 'https://rnchatbot.netlify.app'],
   credentials: true,
 }));
 app.use(express.json());
